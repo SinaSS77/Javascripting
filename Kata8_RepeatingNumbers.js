f758bd8223068be4d58101c5ddc0d076878942f6
SinaSS77 <s.molaverdikhani>
2022-12-15 19:43:40 -0800

    Kata8_RepeatingNumbers

--- Kata8_RepeatingNumbers.js
+++ Kata8_RepeatingNumbers.js
@@ -12,3 +12,18 @@ const repeatNumbers = (data) => {
   }
 return resultArray.join(", ");
 };
+
+console.log(repeatNumbers([[1, 10]]));
+console.log(
+  repeatNumbers([
+    [1, 2],
+    [2, 3],
+  ])
+);
+console.log(
+  repeatNumbers([
+    [10, 4],
+    [34, 6],
+    [92, 2],
+  ])
+);
