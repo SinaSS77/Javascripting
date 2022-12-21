cc1e8ea2bc109eabaaea4fb3446c48f17903cdf5
SinaSS77 <s.molaverdikhani>
2022-12-20 19:01:16 -0800

    object push_was interesting

---
+++ KATA15_OrganizingInstructors.js
@@ -0,0 +1,32 @@
+
+// Kata 15 - Organizing Instructors
+/* In this exercise, we will be given a list of instructors and we will create a single object to organize them based on their course */
+const organizeInstructors = function (instructors) {
+  let result = {};
+  
+  for (let i = 0; i < instructors.length; i++) {
+    if(!result[instructors[i].course]){
+      result[instructors[i].course] = [];
+      result[instructors[i].course].push(instructors[i].name);
+      
+    }else{
+      result[instructors[i].course].push(instructors[i].name);
+
+    }
+  }
+  return result;
+};
+
+console.log(organizeInstructors([
+  {name: "Samuel", course: "iOS"},
+  {name: "Victoria", course: "Web"},
+  {name: "Karim", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
+console.log(organizeInstructors([
+  {name: "Brendan", course: "Blockchain"},
+  {name: "David", course: "Web"},
+  {name: "Martha", course: "iOS"},
+  {name: "Carlos", course: "Web"}
+]));
+
